d63e109657f77adaf53c537a733011a6bb2473aa
Shobhit010 <137671084+Shobhit010@users.noreply.github.com>
2025-04-20 13:05:56 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import IndexPage from './pages/indexpage';
+import IndexPage from './pages/Indexpage';
 import LoginPage from './pages/LoginPage';
 import Layout from './Layout';
 import RegisterPage from './pages/RegisterPage';
